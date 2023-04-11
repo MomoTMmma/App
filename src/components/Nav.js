620f040d11509bc10dc8245a5d013938242d2058
Melissa Arreola <melissaarreola@melissas-mbp.lan>
2023-04-11 00:50:52 -0700

    first

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import './Nav.css';
+
+
+
+const Nav = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand navbar-light bg-dark">
+                <div className="nav navbar-nav">
+                    <Link className="nav-item nav-link active" to='/'>Home</Link>
+                    
+                    <Link className="nav-item nav-link active" to='/shop'>Shop</Link>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Nav;
---
+++ src/views/Shop.js
@@ -0,0 +1,12 @@
+
+
+
+const Shop = () => {
+    return (
+        <div>
+            <h1>Shop Page</h1>
+        </div>
+    );
+}
+
+export default Shop;
